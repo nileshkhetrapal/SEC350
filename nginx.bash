b91c53a19517bfdeb066eaf8eff91f25167075a4
nileshkhetrapal <71113185+nileshkhetrapal@users.noreply.github.com>
2022-03-06 20:40:29 -0500

    Create nginx.bash

---
+++ nginx.bash
@@ -0,0 +1,64 @@
+#!/bin/bash
+#Storyline: Setup Nginx
+function NetCon() {
+#conf="00-installer-config.yaml"
+conf="/etc/netplan/00-installer-config.yaml"
+rm ${conf}
+ip=172.16.50.5
+netmask=29
+gateway=172.16.50.2
+ns=172.16.200.11
+#echo the contents onto a file
+echo "
+network:
+   version: 2
+   renderer: networkd
+   ethernets:
+      ens160:
+         dhcp4: false
+         addresses: [${ip}/${netmask}]
+         gateway4: ${gateway}
+         nameservers:
+           addresses: [${ns}]
+" >> ${conf}
+netplan apply
+}
+function SysCon() {
+hostname="nginx01-nilesh"
+#command to set hostname
+hostname ${hostname}
+}
+function UserCon() {
+uN="Nilesh"
+uP="N!lesh@123"
+adduser ${uN} ${uP}
+adduser ${uN} sudo
+}
+function LogCon() {
+echo "auth,authpriv.*@172.16.200.10:1514;RYSYLOG_SyslogProtocol23Format" >> etc/rsyslog.d/sec350.conf
+systemctl restart rsyslog
+}
+function nginx() {
+apt install nginx -Y
+ufw allow 'Nginx HTTP'
+ufw disable
+ufw enable
+echo "
+<html>
+<head>
+<title>Internal Script</title>
+</head>
+<body>
+<h1>
+   Hello Devin
+</h1>
+</body>
+</html>
+" >> /var/www/html/index.html
+systemtctl restart nginx
+}
+SysCon
+UserCon
+NetCon
+LogCon
+nginx
